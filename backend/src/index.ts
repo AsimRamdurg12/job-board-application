91a4f7e2b697bb49e23738ef3fc00399ea93c50e
AsimRamdurg12 <asimramdurg12@gmail.com>
2025-01-04 13:25:47 +0530

    deployment changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,7 +21,14 @@ cloudinary.config({
 });
 
 app.use(express.json({ limit: "5mb" }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://job-board-application-be.onrender.com"],
+    methods: ["GET", "POST"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
