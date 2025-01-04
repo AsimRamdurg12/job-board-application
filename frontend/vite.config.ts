bc0415533caa491e9b004d00ce2a28ad1c508fb7
AsimRamdurg12 <asimramdurg12@gmail.com>
2025-01-04 15:40:10 +0530

    vite file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,12 +12,13 @@ export default defineConfig({
   // });
   server: {
     host: true,
-    port: 3000,
+    port: 5173,
     proxy: {
       "/api": {
         target: "https://job-board-application-be.onrender.com",
         changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
