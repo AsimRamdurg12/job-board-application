bdc7b42b8e88ea1e5e96013cfc8b794b7c41d6c5
AsimRamdurg12 <asimramdurg12@gmail.com>
2025-01-05 15:51:49 +0530

    init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -28,6 +28,14 @@ app.use(
     credentials: true,
   })
 );
+
+app.options(
+  "*",
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
